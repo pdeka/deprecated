c2a3d5d0e79fdd6bb896b61e5f7b661eb5f4cfbf
pdeka <prabindeka@gmail.com>
2020-03-18 12:19:06 +1100

    Configuration added and pages not needed are removed.

--- src/prismic-configuration.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  // -- Prismic repository GraphQL endpoint
-  apiEndpoint: 'https://developmentsinrealestate.prismic.io/graphql'
-  // apiEndpoint: 'https://nbstyling.prismic.io/graphql'
-};
