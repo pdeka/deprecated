b721a77302fe7e2ef0b62218222962a394a8449e
pdeka <prabindeka@gmail.com>
2020-03-18 11:56:45 +1100

    Initial Revision

---
+++ src/prismic-configuration.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // -- Prismic repository GraphQL endpoint
+  apiEndpoint: 'https://developmentsinrealestate.prismic.io/graphql'
+  // apiEndpoint: 'https://nbstyling.prismic.io/graphql'
+};
