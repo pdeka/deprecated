1c27b79cafad9e4e8d907cd7a05d5dd9b7d0a080
pdeka <prabindeka@gmail.com>
2020-03-19 14:39:41 +1100

    Site config set up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,1 @@
-module.exports = {
-  plugins: [
-    {
-      resolve: `gatsby-theme-mosman`,
-      options: {}
-    }
-  ]
-}
+
--- site-config.js
+++ site-config.js
@@ -1,6 +1,6 @@
-const SiteConfiguration  = require('kenekt-site-configuration');
+const {prodConfiguration}  = require('kenekt-site-configuration');
 const { apiEndpoint } = require('./prismic-configuration');
 
 console.log("Running script to produce site config");
 
-SiteConfiguration().run(apiEndpoint, './.env', './.netlify.toml', 'mosman', 8000)
+prodConfiguration(apiEndpoint, 'mosman', 8000);
