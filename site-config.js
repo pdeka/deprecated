676680a7e993912d81876ca9a0f2ac6dc996e515
pdeka <prabindeka@gmail.com>
2020-03-19 15:11:56 +1100

    site config changes

--- site-config.js
+++ site-config.js
@@ -1,6 +1,12 @@
 const {prodConfiguration}  = require('kenekt-site-configuration');
+const _  = require('lodash');
 const { apiEndpoint } = require('./prismic-configuration');
+const argv = require('yargs').argv;
 
-console.log("Running script to produce site config");
+if (_.isEmpty(argv.theme) || _.includes(["mosman", "narrabeen", "randwick"], argv.theme) === false) {
+  throw new Error('Usage: --theme=mosman . Theme can be mosman, randwick or narrabeen');
+}
 
-prodConfiguration(apiEndpoint, 'mosman', 8000);
+console.log("Running script to produce site config. Theme: " + argv.theme);
+
+prodConfiguration(apiEndpoint, argv.theme, 8001);
