80d782b58cd4dfa0eb32aab6ea5637bab2bf05b2
pdeka <prabindeka@gmail.com>
2020-04-10 11:12:39 +1000

    Changed to add version to the package.json file

--- site-config.js
+++ site-config.js
@@ -3,10 +3,10 @@ const _  = require('lodash');
 const { apiEndpoint } = require('./prismic-configuration');
 const argv = require('yargs').argv;
 
-if (_.isEmpty(argv.theme) || _.includes(["mosman", "narrabeen", "randwick"], argv.theme) === false) {
-  throw new Error('Usage: --theme=mosman . Theme can be mosman, randwick or narrabeen');
+if (_.isEmpty(argv.theme) || _.includes(["mosman", "narrabeen", "randwick"], argv.theme) === false || _.isEmpty(argv.theme-version)) {
+  throw new Error('Usage: --theme=mosman --theme-version="1.0.0" . Theme can be mosman, randwick or narrabeen');
 }
 
 console.log("Running script to produce site config. Theme: " + argv.theme);
 
-prodConfiguration(apiEndpoint, argv.theme, 8000);
+prodConfiguration(apiEndpoint, argv.theme, 8000, argv.theme-version);
