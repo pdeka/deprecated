6997998c14422460bee152a322a8b3a17be62034
pdeka <prabindeka@gmail.com>
2020-04-26 07:06:18 +1000

    These files are dynamically produced and are not needed in the shell

--- site-config.js
+++ site-config.js
@@ -1,12 +1,5 @@
 const {prodConfiguration}  = require('kenekt-site-configuration');
-const _  = require('lodash');
 const { apiEndpoint } = require('./prismic-configuration');
 const argv = require('yargs').argv;
-
-if (_.isEmpty(argv.theme) || _.includes(["mosman", "narrabeen", "randwick"], argv.theme) === false || _.isEmpty(argv.themeversion)) {
-  throw new Error('Usage: --theme=mosman --theme-version="1.0.0" . Theme can be mosman, randwick or narrabeen');
-}
-
-console.log("Running script to produce site config. Theme: " + argv.theme);
 
 prodConfiguration(apiEndpoint, argv.theme, 8000, argv.themeversion);
